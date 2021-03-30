d6b900d887c091649cea5465a5c4e025dd3355ff
roger rogerio <RogerRogeron3@hotmail.com>
2021-03-30 02:19:53 +0000

    Updated Product.cs

--- Product.cs
+++ Product.cs
@@ -15,12 +15,55 @@ namespace Product
 		private double _pricePurchaseProduct;
 		private double _priceSellProduct;
 		#endregion
+		
 		#region "PROPRIEDADES PRODUCT"
-		public int IdProduct;
+		public int IdProduct
 		{
 			get { return _idProduct; }
 			set { _idProduct = value; }
 		}
+		public string NameProduct
+		{
+			get { return _nameProduct; }
+			set { _nameProduct = value; }
+		}
+		public string DescriptionProduct
+		{
+			get { return _descriptionProduct; }
+			set { _descriptionProduct = value; }
+		}
+		public string TypeProduct
+		{
+			get { return _typeProduct; }
+			set { _typeProduct = value; }
+		}
+		public double WeightProduct
+		{
+			get { return _weightProduct; }
+			set { _weightProduct = value; }
+		}
+		public string ProviderProduct;
+		{
+			get { return _providerProduct; }
+			set { _providerProduct = value; }
+		}
+		public bool DiscontinuedProduct
+		{
+			get { return _discontinuedProduct; }
+			bool value = true;
+		}
+		public double PricePurchaseProduct
+		{
+			get { return _pricePurchaseProduct; }
+			set { _pricePurchaseProduct = value; }
+		}
+		public double PriceSellProduct
+		{
+			get { return _priceSellProduct; }
+			set { _priceSellProduct = value; }
+		}
+		#endregion
+
 
 	}
 }
