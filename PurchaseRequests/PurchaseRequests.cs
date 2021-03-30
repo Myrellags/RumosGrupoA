a5d56ca629152c264110ba802e8b085f0966f5dd
Myrellags <myrellags@gmail.com>
2021-03-30 06:33:09 -0300

    Inclusão Classes InputInvoice, PurchaseRequeste, Quotation

---
+++ PurchaseRequests/PurchaseRequests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PurchaseRequests
+{
+    public class PurchaseRequests
+    {
+    }
+}
---
+++ Quotation/Quotation.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Quotation
+{
+    public class Quotation
+    {
+    }
+}
