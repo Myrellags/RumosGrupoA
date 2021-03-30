ef3f5247cb17a8f481e58b6d10c84f273e418753
rogerio ribeiro rogerio <mundoroger@gmail.com>
2021-03-30 19:16:44 +0100

    Construtores Product

---
+++ Product/Product.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Product
+{
+	public class Product
+	{
+		#region "ATRIBUTOS PRODUCT"
+		private int _idProduct;
+		private string _nameProduct;
+		private string _descriptionProduct;
+		private string _typeProduct;
+		private double _weightProduct;
+		private string _providerProduct;
+		private bool _discontinuedProduct;
+		private double _pricePurchaseProduct;
+		private double _priceSellProduct;
+		#endregion
+
+		#region "PROPRIEDADES PRODUCT"
+		public int IdProduct
+		{
+			get { return _idProduct; }
+			set { _idProduct = value; }
+		}
+		public string NameProduct
+		{
+			get { return _nameProduct; }
+			set { _nameProduct = value; }
+		}
+		public string DescriptionProduct
+		{
+			get { return _descriptionProduct; }
+			set { _descriptionProduct = value; }
+		}
+		public string TypeProduct
+		{
+			get { return _typeProduct; }
+			set { _typeProduct = value; }
+		}
+		public double WeightProduct
+		{
+			get { return _weightProduct; }
+			set { _weightProduct = value; }
+		}
+		public string ProviderProduct
+		{
+			get { return _providerProduct; }
+			set { _providerProduct = value; }
+		}
+		public bool DiscontinuedProduct
+		{
+			get { return _discontinuedProduct; }
+			set { _discontinuedProduct = value; }
+		}
+		public double PricePurchaseProduct
+		{
+			get { return _pricePurchaseProduct; }
+			set { _pricePurchaseProduct = value; }
+		}
+		public double PriceSellProduct
+		{
+			get { return _priceSellProduct; }
+			set { _priceSellProduct = value; }
+		}
+		#endregion
+		#region "CONTRUTORES"
+		public Product()
+        {
+
+        }
+		public Product(int idProduct, string nameProduct, string descriptionProduct, string typeProduct, double weightProduct, string providerProduct, bool discontinuedProduct, double pricePurchaseProduct, double priceSellProduct)
+		{
+			_idProduct = idProduct;
+			_nameProduct = nameProduct;
+			_descriptionProduct = descriptionProduct;
+			_typeProduct = typeProduct;
+			_weightProduct = weightProduct;
+		_providerProduct = providerProduct;
+		_discontinuedProduct = discontinuedProduct;
+			_pricePurchaseProduct = pricePurchaseProduct;
+			_priceSellProduct = priceSellProduct;
+
+	}
+		#endregion
+
+	}
+
+}
+
