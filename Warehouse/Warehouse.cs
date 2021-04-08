e82f50ea8ad873cd984d2b7d3d2fc11de2f8b9e3
Myrellags <myrellags@gmail.com>
2021-04-08 15:08:54 -0300

    Criação da Classe Warehouse

---
+++ Warehouse/Warehouse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Warehouse
+{
+    public class Warehouse
+    {
+        #region "Atributos"
+        private string _idWarehouse; //private string _idArmazem
+        private string _descritionWarehouse; //private string _descricaoArmazem
+        #endregion
+
+        #region "Propriedades"
+
+        public string IdWarehouse
+        {
+            get { return _idWarehouse; }
+            set { _idWarehouse = value; }
+        }
+
+        public string DescriptionWarehouse
+        {
+            get { return _descritionWarehouse; }
+            set { _descritionWarehouse = value; }
+        }
+
+        #endregion
+
+        #region "Construtores"
+        public Warehouse()
+        {
+
+        }
+
+        public Warehouse(string idWarehouse, string descriptionWarehouse)
+        {
+            _idWarehouse = idWarehouse;
+            _descritionWarehouse = descriptionWarehouse;
+        }
+        #endregion
+    }
+}
