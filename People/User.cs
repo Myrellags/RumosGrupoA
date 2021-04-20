c88309f79ab4aabb95939f680626347a2e06ac92
Myrellags <myrellags@gmail.com>
2021-04-20 12:40:35 -0300

    Reestruturação das classes parte 2

---
+++ People/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace User
+{
+    public class User
+    {
+        #region "ATRIBUTOS USER" 
+        private int _idUser;
+        private int _idPeople;
+        private string  _passUser;
+        
+
+        #endregion
+        #region "PROPRIEDADES USER"
+        public int IdUser
+        {
+            get { return _idUser; }
+            set { _idUser = value; }
+        }
+        public int idPeople
+        {
+            get { return _idPeople; }
+            set { _idPeople = value; }
+        }
+        public string PassUser
+        {
+            get { return _passUser; }
+            set { _passUser = value; }
+        }
+       
+        #endregion
+        #region "CONSTRUTORES USER"
+        public User()
+        {
+
+        }
+        public User(int idUser, int idPeople, string passUser)
+        {
+            _idUser = idUser;
+            _idPeople = idPeople;
+            _passUser = passUser;
+        }
+        #endregion
+
+    }
+}
+
