22cea6064d6b07fc4ead63dbf3d0665af02dcf0a
Myrellags <myrellags@gmail.com>
2021-05-08 09:47:20 -0300

    WeaAppMVC

---
+++ WebAppMVCEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMVCEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
