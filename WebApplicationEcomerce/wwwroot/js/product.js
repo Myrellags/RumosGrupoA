90ffc12ccf2f7a32cb45e5b641b609d084b0ab9f
Myrellags <myrellags@gmail.com>
2021-05-15 16:19:55 -0300

    Carregar combo categorias

---
+++ WebApplicationEcomerce/wwwroot/js/product.js
@@ -0,0 +1,27 @@
+﻿
+//Carregando combo categorias com dados da base
+document.getElementById("opcaoCategoria").innerHTML = function () {
+    fetch("/ProductDAO/GetProducts").then(respostaDoServidor => respostaDoServidor.json()).then(listaDeCategorias => mostrarCategorias(listaDeCategorias));
+};
+
+function mostrarCategorias(listaDeCategorias) {
+    for (var i = 0; i < listaDeCategorias.length; i++) {
+        document.getElementById("opcaoCategoria").innerHTML +=
+            "<option>" + listaDeCategorias[i] + "</option>";
+    }
+}
+
+function enviar() {
+    if (
+        parseInt(document.getElementById("pesoProduto").value) <= 0 ||
+        parseInt(document.getElementById("pesoProduto").value) > 100
+    ) {
+        alert("Dados inválidos, verificar campos!");
+        return false; 
+    }
+    else {
+        alert("Guardando dados!");
+        return true;
+    }
+}
+
